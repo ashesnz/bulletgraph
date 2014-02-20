9a62f91988a45f31b271bf1b6caf6bd02df45685
Guy Irvine <guy@guyirvine.com>
2014-02-21 05:02:13 +1300

    Initial Import

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-bulletgraph
-===========
+#Bulletgraph
+
+An html version of Stephen Few's [Bullet graph](http://en.wikipedia.org/wiki/Bullet_graph).
+
+
+##Install
+> bower install bulletgraph
+
+
+
+##Usage
+
+> bullet_graph( id, options )
+
+See index.htm for example usage, but this,
+
+>bullet_graph( ".g3",
+             {
+                "ticks": 5,
+                "range": [70,90,100],
+                "measure": 55,
+                "target": 80
+             });
+
+will get you going.
+
+
---
+++ bulletgraph.js
@@ -0,0 +1,66 @@
+function tick( obj, w ) {
+	var innerDiv = document.createElement('div');
+	innerDiv.className = 'tick';
+	innerDiv.style.width = w + "%";
+	obj.appendChild( innerDiv );
+    
+	var innerDiv = document.createElement('div');
+	innerDiv.className = 'tick-label';
+	innerDiv.style.width = ( w * 2 ) + "%";
+	innerDiv.innerHTML = w;
+	obj.appendChild( innerDiv );
+    
+}
+
+function ticks( obj, num ) {
+	var step = 100 / num;
+	for( var i=0;i<100;i=i+step ) {
+		tick( obj, Math.round(i) );
+	}
+	tick( obj, 100 );
+}
+
+function range( obj, list ) {
+	var index = list.length;
+	while( index > 0 ) {
+		var innerDiv = document.createElement('div');
+		innerDiv.className = 'range range-' + index;
+		innerDiv.style.width = list[index-1] + "%";
+		obj.appendChild( innerDiv );
+		
+		index--;
+	}
+    
+}
+
+function measure( obj, num ) {
+	var innerDiv = document.createElement('div');
+	innerDiv.className = 'measure';
+	innerDiv.style.width = num + "%";
+	obj.appendChild( innerDiv );
+}
+
+function target( obj, num ) {
+	var innerDiv = document.createElement('div');
+	innerDiv.className = 'target';
+	innerDiv.style.width = num + "%";
+	obj.appendChild( innerDiv );
+}
+
+
+function bullet_graph( id, options ) {
+    var obj = document.querySelectorAll(id)[0]
+
+    if ( !(options.ticks == undefined) ) {
+        ticks( obj, options.ticks );
+    }
+    if ( !(options.range == undefined) ) {
+        range( obj, options.range );
+    }
+    if ( !(options.measure == undefined) ) {
+        measure( obj, options.measure );
+    }
+    if ( !(options.target == undefined) ) {
+        target( obj, options.target );
+    }
+}
